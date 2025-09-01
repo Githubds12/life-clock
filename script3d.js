1804c91a6fafa0af1a1e94d7f5469155dae8e37e
Deepanshu Singh <deepdude61@gmail.com>
2025-09-01 12:10:24 +0530

    test

--- script3d.js
+++ script3d.js
@@ -2161,7 +2161,6 @@ window.onYouTubeIframeAPIReady = function () {
   player = new YT.Player("youtube-player", {
     videoId: "IvjMgVS6kng", // Lofi Girl - Lofi hip hop radio. This is a very popular and stable video.
     playerVars: {
-      autoplay: 0,
       controls: 1,
       autoplay: 1,
       loop: 1,
